4e5675bc648ca227594d419763840a1c73694c4b
Nick <nicolas.wulk@gmail.com>
2022-12-03 14:28:57 -0300

    Corrigindo bot.js

--- bot.js
+++ bot.js
@@ -8,10 +8,10 @@ console.log("Bot running")
 
 //autenticação
 const T = new Twit({
-    consumer_key: TWITTER_API_KEY,
-    consumer_secret: TWITTER_API_SECRET,
-    access_token: TWITTER_ACCESS_TOKEN,
-    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+    consumer_key: process.env.TWITTER_API_KEY,
+    consumer_secret: process.env.TWITTER_API_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   })
 
 //cálculo do dia de hoje
