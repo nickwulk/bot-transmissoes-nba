8a654d482a65532bbb8ce11488a849227ce27958
Nick <nicolas.wulk@gmail.com>
2022-12-04 18:08:05 -0300

    Teste fluxo CI 17

--- bot.js
+++ bot.js
@@ -9,9 +9,9 @@ console.log("Bot running")
 //autenticação
 const T = new Twit({
     consumer_key: process.env.TWITTER_API_KEY,
-    consumer_secret: secrets.TWITTER_API_SECRET,
-    access_token: secrets.TWITTER_ACCESS_TOKEN,
-    access_token_secret: secrets.TWITTER_ACCESS_TOKEN_SECRET,
+    consumer_secret: process.env.TWITTER_API_SECRET,
+    access_token: process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
   })
 
 //cálculo do dia de hoje
